6e9f905e13789c93351868aec575877bb1a96b2a
zoeychenrui <zoeychen2013@gmail.com>
2025-01-30 22:59:02 -0500

    node adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,7 @@
-import adapter from '@sveltejs/adapter-node';  // Import the Node adapter
-import { defineConfig } from 'vite';
+import adapter from '@sveltejs/adapter-node';
 
-export default defineConfig({
-  kit: {
-    adapter: adapter(), // Use the Node adapter
-    target: '#svelte',   // This is typically the default, ensuring SSR works
-    // Additional configurations can go here
-  },
-});
+export default {
+	kit: {
+		adapter: adapter()
+	}
+};
