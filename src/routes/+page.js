1ae908ced2735b0ff29ea8d4e18a5a42a37fa90f
zoeychenrui <zoeychen2013@gmail.com>
2025-01-30 23:44:19 -0500

    relative path

--- src/routes/+page.js
+++ src/routes/+page.js
@@ -1,5 +1,5 @@
 export async function load({ fetch }) {
-    const res = await fetch("http://localhost:5001/api/dictionary");
+    const res = await fetch("/api/dictionary");
 
     if (!res.ok) {
         throw new Error("Failed to fetch dictionary data");
--- svelte.config.js
+++ svelte.config.js
@@ -3,8 +3,5 @@ import adapter from '@sveltejs/adapter-node';
 export default {
 	kit: {
 		adapter: adapter(),
-		paths: {
-			base: '/dictionary-s96x',  // Ensure this matches the path in Render
-		  },
 	}
 };
