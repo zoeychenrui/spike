d09d80d83b21c9914f8e4000bf10b750ff2fef20
zoeychenrui <zoeychen2013@gmail.com>
2025-01-30 23:10:57 -0500

    render path

--- svelte.config.js
+++ svelte.config.js
@@ -2,6 +2,9 @@ import adapter from '@sveltejs/adapter-node';
 
 export default {
 	kit: {
-		adapter: adapter()
+		adapter: adapter(),
+		paths: {
+			base: '/dictionary-s96x',  // Ensure this matches the path in Render
+		  },
 	}
 };
